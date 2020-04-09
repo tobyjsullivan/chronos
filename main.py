bb23f3fde862256183f6f6b2e86c2930dd473204
Toby Sullivan <tobyjsullivan@gmail.com>
2020-04-09 09:18:28 -0700

    Parse timestamp from arg

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+from datetime import datetime
+import sys
 import time
 
-def main():
-  now = time.time()
-  print("Wall time: " + time.ctime(now))
+def main(args):
+  if len(args) > 1:
+    now = int(args[1])
+  else:
+    now = time.time()
+
+  dt = datetime.utcfromtimestamp(now)
+  print("Wall time (UTC): " + dt.isoformat(' '))
 
   hours = int(now / 3600)
   print("Hour: ", hours)
 
-main()
+
+main(sys.argv)
